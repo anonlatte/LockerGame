5805af189d59b0310fc4afe35abfb4803afc374d
Herman Proshunin <g.proshunin15@gmail.com>
2026-04-25 01:42:16 +0500

    Fix vibro

--- app/src/main/java/com/example/lockergame/feature/game/Haptics.kt
+++ app/src/main/java/com/example/lockergame/feature/game/Haptics.kt
@@ -1,15 +1,16 @@
 package com.example.lockergame.feature.game
 
-import android.content.Context
 import android.os.Build
 import android.os.VibrationEffect
 import android.os.Vibrator
+import android.os.VibratorManager
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.hapticfeedback.HapticFeedback
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.core.content.ContextCompat.getSystemService
 
 interface HapticPerformer {
     fun performSubtleTick()
@@ -22,17 +23,12 @@ private class ComposeHapticPerformer(
 ) : HapticPerformer {
     override fun performSubtleTick() {
         hapticFeedback.performHapticFeedback(HapticFeedbackType.TextHandleMove)
+        vibrator?.vibrate(VibrationEffect.createOneShot(1L, 1))
     }
 
     override fun performUnlock() {
         hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
-        vibrator ?: return
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            vibrator.vibrate(VibrationEffect.createOneShot(50L, 180))
-        } else {
-            @Suppress("DEPRECATION")
-            vibrator.vibrate(50L)
-        }
+        vibrator?.vibrate(VibrationEffect.createOneShot(50L, 60))
     }
 }
 
@@ -41,7 +37,11 @@ fun rememberHapticPerformer(): HapticPerformer {
     val context = LocalContext.current
     val haptic = LocalHapticFeedback.current
     return remember(context, haptic) {
-        val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as? Vibrator
+        val vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            getSystemService(context, VibratorManager::class.java)?.defaultVibrator
+        } else {
+            getSystemService(context, Vibrator::class.java)
+        }
         ComposeHapticPerformer(haptic, vibrator)
     }
 }
